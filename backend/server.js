a9138bc3bb2ab9e0f5b4e43e11ee21a4e92af702
shivaniSshekhawat <shivanishekhawat2694@gmail.com>
2026-01-28 23:31:40 +0530

    *fix

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 app.use(router);
 app.use("/", express.static(path.join(__dirname, "dist")));
 
-app.get("(.*)", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
