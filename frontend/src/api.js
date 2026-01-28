1aab84c6f905d870dbe9e96cf2e0114a3d3ad3ed
shivaniSshekhawat <shivanishekhawat2694@gmail.com>
2026-01-28 22:36:57 +0530

    *fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000"
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://day-19-gfg-pay-alx3.vercel.app"
 });
 
 export const getShows = () => api.get("/shows");
